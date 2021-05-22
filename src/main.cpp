18aa488c50f540dce01f42d36b4eab6f2d493a62
Gioel Adriano Vencato <81482824+Gioo96@users.noreply.github.com>
2021-05-22 22:35:50 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
-#include "tracker.cpp"
-
+#include "tracking.cpp"
 
 using namespace std;
 using namespace cv;
 
+// Function's declaration
+void onMouse(int event, int x, int y, int flags, void* param);
+vector<Point2f> sort_corners(vector<Point2f> corners, int cols, int rows);
+
+// Global variables
+vector<Point2f> object_corners;
+int num_corners = 0;
+
 int main(int argc, const char * argv[]) {
 
-    String path = "/Users/gioel/Documents/Control\ System\ Engineering/Computer\ Vision/Lab_6/data/video.mov";
+    String path = "/Users/gioel/Documents/Control System Engineering/Computer Vision/Lab_6/data/video.mov";
     // vector of frames
     vector<Mat> frames;
     VideoCapture cap(path);
@@ -32,14 +39,111 @@ int main(int argc, const char * argv[]) {
     String dataset_path = "/Users/gioel/Documents/Control System Engineering/Computer Vision/Lab_6/data/objects/*.png";
     Tracking track(frames, dataset_path);
     
+    // Visualize good keypoints
     vector<vector<KeyPoint>> list_keypoints_dataset;
     vector<Mat> list_descriptors_dataset;
     vector<KeyPoint> keypoints_frame;
     Mat descriptors_frame;
     vector<Mat> H;
-    track.visualizeGoodKeypoints(list_keypoints_dataset, list_descriptors_dataset, keypoints_frame, descriptors_frame, H);
     
-    track.drawRect(H);
+    vector<vector<Point2f>> allcoords_keypoints;
+    allcoords_keypoints = track.visualizeGoodKeypoints(list_keypoints_dataset, list_descriptors_dataset, keypoints_frame, descriptors_frame, H);
+    
+    // Get corners of each object in the images of the dataset
+    vector<vector<Point2f>> tot_corners;
+    for (int i=0; i<track.images_dataset.size(); i++) {
+        
+        String window_name = "Object n." + to_string(i+1);
+        namedWindow(window_name);
+        Mat* image = &track.images_dataset.at(i);
+        imshow(window_name, *image);
+        setMouseCallback(window_name, onMouse, static_cast<void*>(image));
+        waitKey(0);
+
+        // Sort corners
+        vector<Point2f> corners_sorted;
+        corners_sorted = sort_corners(object_corners, track.images_dataset.at(i).cols, track.images_dataset.at(i).rows);
+        
+        tot_corners.push_back(corners_sorted);
+        
+    }
+
+    track.drawRect(H, tot_corners);
+    track.trackObjects(allcoords_keypoints);
+    
+    
 
     return 0;
 }
+
+
+void onMouse(int event, int x, int y, int flags, void* userdata) {
+    
+    if (event != EVENT_LBUTTONDOWN) {
+   
+        return;
+    }
+    else {
+        
+        if (num_corners == 4) {
+    
+            vector<Point2f> init;
+            object_corners = init;
+            num_corners = 0;
+        }
+        
+        Point2f corner(x,y);
+        object_corners.push_back(corner);
+        num_corners ++;
+        
+        cout<<"Corner n." + to_string(num_corners) + ":"<<corner<<endl;
+        
+        if (num_corners == 4) {
+            
+            cout<<"Press a key"<<endl;
+        }
+    }
+}
+
+vector<Point2f> sort_corners(vector<Point2f> corners, int cols, int rows) {
+    
+    vector<Point2f> corners_sorted;
+    vector<int> j; // j will contain the index of corners to be add at the end of corners_sorted
+    int i = 0;
+    
+    // Find not correct corners index
+    while (i < corners.size()-1) {
+        
+        if (abs(corners.at(i).x - corners.at(i+1).x) > cols/2 && abs(corners.at(i).y - corners.at(i+1).y) > rows/2) {
+            j.push_back(i+1);
+        }
+        i ++;
+    }
+
+    // If the index of a corner is correct then add the corner to corners_sorted
+    for (i=0; i<corners.size(); i++) {
+        
+        bool found = false;
+        for (int k=0; k<j.size(); k++) {
+            
+            if (i == j.at(k)) {
+                
+                found = true;
+            }
+        }
+        if (!found) {
+            
+            corners_sorted.push_back(corners.at(i));
+        }
+    }
+    
+    // Add all the remaining corners at the end of corners_sorted
+    for (i=0; i<j.size(); i++) {
+        
+        corners_sorted.push_back(corners.at(j.at(i)));
+    }
+    
+    return corners_sorted;
+}
+
+
---
+++ src/tracking.cpp
@@ -0,0 +1,223 @@
+#include "tracking.h"
+
+using namespace cv;
+using namespace std;
+
+// PanoramicImage Class
+// constructor
+Tracking::Tracking(vector<Mat> images_f, String dataset_path) {
+
+    images_frame = images_f;
+        
+    vector<String> fn;
+    glob(dataset_path, fn, false);
+    if (!fn.size()) {
+        
+        cout<<"Error loading the dataset"<<endl;
+        return;
+    }
+    else {
+        
+        size_t count = fn.size(); // Number of images
+        for (size_t i = 0; i < count; i++) {
+            
+            images_dataset.push_back(imread(fn[i], IMREAD_COLOR));
+        }
+    }
+}
+
+vector<vector<Point2f>> Tracking::visualizeGoodKeypoints(vector<vector<KeyPoint>> &list_keypoints_dataset, vector<Mat> &list_descriptors_dataset, vector<KeyPoint> &keypoints_frame, Mat &descriptors_frame, vector<Mat> &H) {
+    
+    // COMPUTE KEYPOINTS & DESCRIPTORS
+    Ptr<SIFT> sift = SIFT::create();
+    
+    // Keypoints & descriptors foreach image of the dataset
+    for (int i = 0; i < images_dataset.size(); i ++) {
+        
+        vector<KeyPoint> keypoints_d;
+        Mat descriptors_d;
+        sift->detectAndCompute(images_dataset.at(i), Mat(), keypoints_d, descriptors_d);
+        list_keypoints_dataset.push_back(keypoints_d);
+        list_descriptors_dataset.push_back(descriptors_d);
+    }
+    
+    // Keypoints & descriptors of the first video frame
+    sift->detectAndCompute(images_frame.at(0), Mat(), keypoints_frame, descriptors_frame);
+    
+    // GET GOOD KEYPOINTS
+    vector<vector<KeyPoint>> allgood_keypoints;
+    vector<vector<Point2f>> allcoords_keypoints;
+    Ptr<BFMatcher> matcher = BFMatcher::create(NORM_L2, true);
+    for (int i = 0; i < images_dataset.size(); i ++) {
+        
+        // Get all matches of first video frame and image 'i' of the dataset
+        vector<DMatch> matches;
+        matcher->match(descriptors_frame, list_descriptors_dataset.at(i), matches);
+        
+        // Get minimum distance between descriptors
+        double min_distance = 200;
+        for (int j = 0; j < matches.size(); j++) {
+
+            //if (matches.at(j).distance < min_distance && matches.at(j).distance > 0) {
+            if (matches.at(j).distance < min_distance) {
+
+                min_distance = matches.at(j).distance;
+            }
+        }
+        
+        // Refine matches
+        vector<DMatch> refined_matches;
+        for (int j = 0; j < matches.size(); j++) {
+
+            if (matches.at(j).distance < 3 * min_distance) {
+
+                refined_matches.push_back(matches.at(j));
+            }
+        }
+        
+        // Good matches (RANSAC)
+        vector<Point2f> scene, object;
+        for (int j = 0; j < refined_matches.size(); j++) {
+            
+            // Refined matches between the dataset image and first framed img (Pixel cords)
+            scene.push_back(keypoints_frame.at(refined_matches.at(j).queryIdx).pt);
+            object.push_back(list_keypoints_dataset.at(i).at(refined_matches.at(j).trainIdx).pt);
+        }
+        
+        Mat H_single;
+        vector<int> mask; // mask will contain 0 if the match is wrong
+        H_single = findHomography(object, scene, mask, RANSAC);
+        H.push_back(H_single);
+
+        // Good matches
+        vector<DMatch> good_matches;
+        for (int j=0; j<mask.size(); j++) {
+    
+            if (mask.at(j) != 0) {
+    
+                good_matches.push_back(refined_matches.at(j));
+            }
+        }
+        
+        // Good keypoints
+        vector<KeyPoint> good_keypoints;
+        vector<Point2f> coords_keypoints;
+        
+        for (int j=0; j<good_matches.size(); j++) {
+    
+            good_keypoints.push_back(keypoints_frame.at(good_matches.at(j).queryIdx));
+            coords_keypoints.push_back(good_keypoints.at(j).pt);
+        }
+        
+        allgood_keypoints.push_back(good_keypoints);
+        allcoords_keypoints.push_back(coords_keypoints);
+    }
+    
+    // Visualize good keypoints
+    vector<Scalar> color = {Scalar(0,0,255), Scalar(255,0,0), Scalar(0,255,0), Scalar(50,50,50)};
+    Mat img_keypoints = images_frame.at(0);
+    for (int i=0; i<images_dataset.size(); i++) {
+        
+        drawKeypoints(img_keypoints, allgood_keypoints.at(i), img_keypoints, color.at(i));
+    }
+    imshow("Visualize good keypoints", img_keypoints);
+    waitKey(0);
+    
+    return allcoords_keypoints;
+}
+
+void Tracking::drawRect(vector<Mat> H, vector<vector<Point2f>> vec_corners) {
+ 
+    // Corners of each image of the dataset
+//    vector<Point2f> obj_corners(4);
+//
+//    obj_corners[0] = Point2f(0, 0);
+//    obj_corners[1] = Point2f(static_cast<float>(images_dataset.at(0).cols), 0);
+//    obj_corners[2] = Point2f(static_cast<float>(images_dataset.at(0).cols), static_cast<float>(images_dataset.at(0).rows));
+//    obj_corners[3] = Point2f(0, static_cast<float>(images_dataset.at(0).rows));
+         
+    vector<Scalar> color = {Scalar(0,0,255), Scalar(255,0,0), Scalar(0,255,0), Scalar(50,50,50)};
+    for (int i=0; i<H.size(); i++) {
+    
+        // Compute corrispondent pixel in frame image
+        vector<Point2f> scene_corners(4);
+        perspectiveTransform(vec_corners.at(i), scene_corners, H.at(i));
+    
+        // Draw lines
+        line(images_frame.at(0), scene_corners[0], scene_corners[1], color.at(i), 3);
+        line(images_frame.at(0), scene_corners[1], scene_corners[2], color.at(i), 3);
+        line(images_frame.at(0), scene_corners[2], scene_corners[3], color.at(i), 3);
+        line(images_frame.at(0), scene_corners[3], scene_corners[0], color.at(i), 3);
+        imshow("Object detection n. " + to_string(i+1), images_frame.at(0));
+        cout<<"----------"<<endl<<"Detected object n. " + to_string(i+1)<<endl<<"Press a key"<<endl;
+        waitKey(0);
+    }
+}
+
+void Tracking::trackObjects(vector<vector<Point2f>> allcoords_keypoints) {
+    
+    int size_video = images_frame.size();
+    vector<Mat> final_frame(size_video);
+    vector<Scalar> color = {Scalar(0,0,255), Scalar(255,0,0), Scalar(0,255,0), Scalar(50,50,50)};
+    
+    Mat gray, prevGray, image, first_image_gray;
+    //vector<vector<Point2f>> points_eachframe;
+
+    cvtColor(images_frame.at(0), first_image_gray, COLOR_BGR2GRAY);
+    TermCriteria termcrit(TermCriteria::MAX_ITER|TermCriteria::EPS,20,0.03);
+
+    Size subPixWinSize(10,10), winSize(10,10);
+    int win_size = 10;
+    
+//    vector<vector<Point2f>> allPrevPoints;
+//    for (int i = 0; i < images_dataset.size(); i++){
+//        vector<Point2f> prevPoints(allcoords_keypoints.at(i).size());
+//        prevPoints = allcoords_keypoints.at(i);
+//        allPrevPoints.push_back(prevPoints);
+//    }
+    
+    for (int i = 1; i < images_frame.size(); i++) {
+        
+        vector<uchar> status;
+        vector<float> err;
+        
+        //Copy each frame on image and save on gray the single channel image
+        images_frame.at(i).copyTo(image);
+        cvtColor(image, gray, COLOR_BGR2GRAY);
+     
+        
+        vector<vector<Point2f>> allNextPoints;
+        
+        for (int j = 0; j < images_dataset.size(); j++){
+            
+            vector<Point2f> nextPoints;
+            
+            cornerSubPix(first_image_gray, allcoords_keypoints.at(j), Size(win_size, win_size), Size(-1,-1), termcrit);
+            calcOpticalFlowPyrLK(first_image_gray, gray, allcoords_keypoints.at(j), nextPoints, status, err, Size( win_size*2+1, win_size*2+1 ), 5, termcrit);
+            
+            allNextPoints.push_back(nextPoints);
+        
+            for (int k = 0; k < allcoords_keypoints.at(j).size(); k++){
+                
+                //circle(first_image_gray, (allPrevPoints.at(j)).at(k), 3, color.at(j));
+                circle(image, (allNextPoints.at(j)).at(k), 3, color.at(j));
+            }
+            
+            
+//            for (int h = 0; h < allcoords_keypoints.at(j).size(); h++){
+//
+//                if (!status[h]){
+//
+//                    allcoords_keypoints.at(j)[h] = allNextPoints.at(j)[h];
+//                }
+//            }
+        }
+        
+        first_image_gray = gray;
+        
+        final_frame.at(i) = image;
+        imshow("Video", final_frame.at(i));
+        waitKey(1);
+    }
+    
+}
